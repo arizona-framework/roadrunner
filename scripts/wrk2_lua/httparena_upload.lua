ab6fb9e7fb032c96fb5a7d72f5608c3e2e409aac
williamthome <williamthome@hotmail.com>
2026-05-12 11:51:18 -0300

    Refresh wrk2_bench.sh PEAK estimates after concat / iodata rounds

--- scripts/wrk2_lua/httparena_upload.lua
+++
@@ -1,6 +0,0 @@
--- POST /upload with 20 MB of 'x' body.
--- Mirrors `build_request(httparena_upload_20mb_*)` in scripts/bench.escript;
--- shared between the `_auto` and `_manual` scenarios.
-wrk.method = "POST"
-wrk.headers["Content-Type"] = "application/octet-stream"
-wrk.body = string.rep("x", 20 * 1024 * 1024)
